c9fbcfd7f699a403dba096c39eeab3df20fe81ee
Rohith H <146511008+Rohith2003664333@users.noreply.github.com>
2024-10-09 14:27:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -260,5 +260,4 @@ def get_crime_alert():
     return jsonify({'alert': crime_alert})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=False)
