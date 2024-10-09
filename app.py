796ed28c2cacef639c3c042fcff15ce375531ada
Rohith H <146511008+Rohith2003664333@users.noreply.github.com>
2024-10-09 21:58:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,6 +144,7 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    username = session.get('username', 'Guest')
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -155,7 +156,7 @@ def login():
             session['username'] = user['username']
             session['mobile'] = user['mobile']
             # Send success response along with the username
-            return jsonify({'success': True, 'username': user['username']})
+            return jsonify({'success': True, 'username': username]})
             #, 'username': user['username']
         else:
             return jsonify({'success': False, 'message': 'Invalid credentials!'})
